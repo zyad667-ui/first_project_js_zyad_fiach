94df795d7fc1c66531da9218cbfdebf3e103e07e
zyad667-ui <ziadfiach4@gmail.com>
2025-04-18 11:39:52 +0100

    exit_part_solution

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+
+let choose = prompt(" do you want to signing up, logging in, or changing the password.")
+
+if (choose="exit") {
+    console.log("you exit successfully ");
+    alert("you exit successfully ")
+    prompt("do you want to  signing up, logging in, or changing the password.")
+}
+
+
+
+
+
+
